df30e480d10c830f846c207bfd78dcee9512d825
Ivan Danilyuk <werwetsad@gmail.com>
2022-09-10 21:29:26 +0500

    Добавлен класс с генерацией массивов целых чисел. Нужно вносить правки в Женин код (абсолютный путь из системы, мейн, ещё что-то)

--- Nikolaev_Lab/Nikolaev_Lab/Matrix.cs
+++ Nikolaev_Lab/Nikolaev_Lab/Matrix.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
-
+ 
 namespace MatrixExperiment
 {
-    public class SquareMatrix
+    static class Launcher
     {
-        public int Size { get; set; }
-        public double[,] Elems { get; set; }
-    }
-
-    class Program
-    {
-        static void Main()
-        {
+        public static void StartSecondTask()
+            {
             for (int n = 1; n <= 1000; n++)
             {
                 Console.WriteLine($"{n} ДАННЫХ\n");
@@ -34,7 +28,7 @@ namespace MatrixExperiment
                     Console.WriteLine($"Время за {i + 1} выполнение программы: {curTime} мкс.");
                 }
 
-                double middleTime = FindMiddleTime(timeElems);
+                double middleTime = Program.FindMiddleTime(timeElems);
 
                 Console.WriteLine($"\nСреднее время выполнения: {middleTime} мкс.\n");
                 Console.WriteLine("----------------------------------------------\n");
@@ -42,21 +36,34 @@ namespace MatrixExperiment
                 FileWriter.WriteData(middleTime, n);
             }
         }
-
-        static double FindMiddleTime(List<double> allElems)
+    }
+    public class SquareMatrix
+    {
+        public int Size { get; set; }
+        public double[,] Elems { get; set; }
+    }
+ 
+    class Program
+    {
+        static void Main()
+        {
+            Launcher.StartSecondTask();
+        }
+ 
+        public static double FindMiddleTime(List<double> allElems)
         {
             allElems.Sort();
             double sumTime = 0;
             List<double> selectedElems = new List<double>();
-
-            for (int i = 0; i < allElems.Count - 2; i++)
+ 
+            for (int i = 0; i < allElems.Count-2; i++)
             {
-                double curDist = allElems[i + 1] - allElems[i];
-                double nextDist = allElems[i + 2] - allElems[i + 1];
-
+                double curDist = allElems[i+1] - allElems[i];
+                double nextDist = allElems[i+2] - allElems[i+1];
+ 
                 if (curDist == 0) ++curDist;
                 if (curDist == 0) ++curDist;
-
+ 
                 if (curDist > nextDist * 5)
                 {
                     if (i < 2)
@@ -65,7 +72,7 @@ namespace MatrixExperiment
                     else
                         for (int j = 0; j <= i; j++)
                             selectedElems.Add(allElems[j]);
-
+ 
                     foreach (double elem in selectedElems) sumTime += elem;
                     return sumTime / selectedElems.Count;
                 }
@@ -77,31 +84,31 @@ namespace MatrixExperiment
                     else
                         for (int j = 0; j <= i + 1; j++)
                             selectedElems.Add(allElems[j]);
-
+ 
                     foreach (double elem in selectedElems) sumTime += elem;
                     return sumTime / selectedElems.Count;
                 }
             }
-
+ 
             foreach (double elem in allElems) sumTime += elem;
             return sumTime / allElems.Count;
         }
     }
-
+ 
     public static class MatrixHelper
     {
         public static SquareMatrix GenerateSquareMatrix(int size)
         {
             double[,] elems = new double[size, size];
             Random rn = new Random();
-
+ 
             for (int i = 0; i < size; i++)
                 for (int j = 0; j < size; j++)
                     elems[i, j] = rn.Next(9);
-
+ 
             return new SquareMatrix { Size = size, Elems = elems };
         }
-
+ 
         public static void PrintMatrix(SquareMatrix matrix)
         {
             for (int i = 0; i < matrix.Size; i++)
@@ -113,29 +120,29 @@ namespace MatrixExperiment
                 }
             }
         }
-
+ 
         public static SquareMatrix MulMatrixes(SquareMatrix matrix1, SquareMatrix matrix2)
         {
             int size = matrix1.Size;
             double[,] muledElems = new double[size, size];
-
+ 
             for (int i = 0; i < size; i++)
                 for (int j = 0; j < size; j++)
                     for (int k = 0; k < size; k++)
                         muledElems[i, j] += matrix1.Elems[i, k] * matrix2.Elems[k, j];
-
+ 
             return new SquareMatrix { Size = size, Elems = muledElems };
         }
     }
-
+ 
     public static class FileWriter
     {
         public static void WriteData(double time, int n)
         {
             string path = Path.Combine(Environment.CurrentDirectory, "OutputedData.csv");
             string line = $"{n};{time / 10.0}\n";
-
-
+ 
+ 
             File.AppendAllText(path, line);
         }
     }
