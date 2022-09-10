35c498469466293b71b6fd2d05b7b62edbfb7c00
Ivan Danilyuk <werwetsad@gmail.com>
2022-09-10 21:29:26 +0500

    Добавлен класс с генерацией массивов целых чисел. Нужно вносить правки в Женин код (мейн, ещё что-то)

---
+++ Nikolaev_Lab/Nikolaev_Lab/ArrGenClass.cs
@@ -0,0 +1,20 @@
+<<<<<<< Updated upstream
+
+=======
+namespace ArrGen
+{
+    public class ArrayGenerator
+    {
+        public static int[] GenerateArray(int length, int maxElem)
+        {
+            int[] array = new int[length];
+            Random rnd = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = rnd.Next(1, maxElem);
+            }    
+            return array;
+        }
+    }
+}
+>>>>>>> Stashed changes
